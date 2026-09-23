7fb2058eec57db21a617ef9ad63554329561373c
Brinkley <1orbrinkley@seattleschools.org>
2026-09-23 11:49:46 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,33 @@
+public class NoSunshine 
+{
+   public static void main(String[] args)
+   {
+   aint("");
+   
+   away();
+   
+   
+   }
+   
+   public static void aint()
+   {
+   System.out.println("Ain't no sunshine when she's gone");
+   
+   public static void aint(String no)
+   {
+   aint();
+   System.out.println("It's not warm when she's away");
+
+   
+   }
+
+   public static void away()
+   {
+   System.out.println("Anytime she goes away");
+   
+   }
+   
+   
+   
+   
+}
