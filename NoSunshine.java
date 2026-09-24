4464e575b4ec0cf0d9ce8b7328d67ae6a90c39c5
Brinkley <1orbrinkley@seattleschools.org>
2026-09-24 13:23:30 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -8,12 +8,30 @@ public class NoSunshine
    System.out.println("Wonder if she's gone away");
    aint("And this house just ain't no home");
    away();
-   System.out.println("And I know, I know, I know, I know I know, I know, I know, I know, I know I know, I know, I know, I know, I know I know, I know, I know, I know, I know I know, I know, I know, I know, I know I know, I know Hey, I ought to leave young thing alone But ain't no sunshine when she's gone, whoa-whoa Ain't no sunshine when she's gone Only darkness every day Ain't no sunshine when she's gone And this house just ain't no home Anytime she goes away Anytime she goes away Anytime she goes away Anytime she goes away");
-   
-
+   space();
+   print("And I know, I know, I know, I know");
+   print("I know, I know, I know, I know, I know");
+   print("I know, I know, I know, I know, I know");
+   print("I know, I know, I know, I know, I know");
+   print("I know, I know, I know, I know, I know");
+   print("I know, I know");
+   space();
+   print("Hey, I ought to leave young thing alone"); 
+   print("But ain't no sunshine when she's gone, whoa-whoa");
+   aint("Only darkness every day");
+   space();
+   aint("And this house just ain't no home"); 
+   space();
+   away();
+   away();
+   away();
+   away();
    }
 
-   
+   public static void print(String text2)
+   {
+   System.out.println(text2);
+   }
    public static void aint(String text1)
    
    {
